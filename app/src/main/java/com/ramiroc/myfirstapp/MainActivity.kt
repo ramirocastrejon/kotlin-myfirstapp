7fb56956ee67396b9e741f65ee182f981ffd4687
ramiro <ramiro.castrejon.jr@gmail.com>
2022-03-08 17:19:18 -0600

    03-07-22

--- app/src/main/java/com/ramiroc/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/ramiroc/myfirstapp/MainActivity.kt
@@ -5,13 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+       /* class MyOnClick : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                sendMessage(button)
+            }
+        }*/
+
+        button.setOnClickListener { sendMessage(button) }
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onPause got called!", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onStop got called!", Toast.LENGTH_LONG).show()
+    }
+
+
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
@@ -23,4 +50,6 @@ class MainActivity : AppCompatActivity() {
         }
         startActivity(intent)
     }
+
 }
+
